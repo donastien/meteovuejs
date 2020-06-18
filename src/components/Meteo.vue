cef5b5e9c0a8880e935547b6da8cb1432decd9c3
donastien <donastien.karoum@gmail.com>
2020-06-18 18:27:34 +0200

    Installed API Weather

--- src/components/Meteo.vue
+++ src/components/Meteo.vue
@@ -12,17 +12,23 @@
         v-on:keypress="goMeteo"
       />
     </div>
-    <div class="w-75 m-auto">
-      <h3 class="text-center mb-3">Position: Paris</h3>
+    <div class="w-75 m-auto" v-if="temps">
+      <h3 class="text-center mb-3">Position: {{ temps.name }}</h3>
       <div class="card text-center p-5">
-        <p class="text-affichage">Temperature: 20°</p>
-        <p class="texte-affichage">Temps : Nuageux</p>
+        <p class="texte-affichage">
+          Température : {{ temps.main.temp.toFixed() }}°
+        </p>
+        <p class="texte-affichage">
+          Temps : {{ temps.weather[0].description }}
+        </p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Meteo',
   data() {
@@ -30,10 +36,31 @@ export default {
       requete: '',
       temps: undefined,
       api_code: '9f296d1ca18093e4dc31d6ade8221ce1',
-      url_recherche: 'api.openweathermap.org/data/2.5/weather?',
+      url_recherche: 'https://api.openweathermap.org/data/2.5/weather?',
     };
+  },
+  methods: {
+    goMeteo(e) {
+      if (e.key == 'Enter') {
+        axios
+          .get(
+            `${this.url_recherche}q=${this.requete}&units=metric&APPID=${this.api_code}&lang=fr`
+          )
+          .then((reponse) => {
+            //console.log(reponse);
+            this.temps = reponse.data;
+          });
+        this.requete = '';
+      }
+    },
   },
 };
 </script>
 
-<style></style>
+<style>
+.texte-affichage {
+  font-size: 30px;
+  font-weight: 300;
+  line-height: 1.2;
+}
+</style>
